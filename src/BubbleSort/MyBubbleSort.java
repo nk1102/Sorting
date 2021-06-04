cbc4d2796ac0709f8d5c5695520a0441458e0c1d
Nikunj <nikunjkhandelwal4614@gmail.com>
2021-06-04 19:42:46 +0530

    Created class My bubbleSort and added main method and taken input from the scanner class

---
+++ src/BubbleSort/MyBubbleSort.java
@@ -0,0 +1,12 @@
+package BubbleSort;
+
+import java.util.Scanner;
+
+public class MyBubbleSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int[] arr = new int[size] ;
+
+    }
+}
