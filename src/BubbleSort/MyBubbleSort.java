8b52cfa5944a3000af921c7f556ecd81ac8af395
Nikunj <nikunjkhandelwal4614@gmail.com>
2021-06-04 20:01:16 +0530

    completed bubble sort algorithm

--- src/BubbleSort/MyBubbleSort.java
+++ src/BubbleSort/MyBubbleSort.java
@@ -7,28 +7,41 @@ public class MyBubbleSort {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
-        int[] arr = new int[size] ;
+        int[] arr = new int[size];
         MyBubbleSort object = new MyBubbleSort();
-        System.out.println("Sorted Array ------>>"+ Arrays.toString(arr));
+        System.out.println("Sorted Array ------>>" + Arrays.toString(arr));
     }
-    public void bubbleSort(int[] arr){
+
+    public void bubbleSort(int[] arr) {
         /**
          *  Time Complexity of this bubble sort algorithm is O(n^2)
          */
         // Step 1 of the algorithm is given below
         for (int i = 0; i < arr.length; i++) {
-            /**
-             * this loop will run the times the array of length
-             * i.e it will run N times
-             */
-            for (int j = 0; j <arr.length-1 ; j++) {
-                /**
-                 * This loop will run length-1 times of the array length
-                 * i.e it will run n-1 times
-                 */
-            }
 
-            
+            // step 2 of the algorithm is given below
+            for (int j = 0; j < arr.length - 1; j++) {
+
+                // step 3 of the algorithm is given below
+                if (arr[j] > arr[j + 1]) {  // this if statement will check the element and element+1 and swap them if previous one is lower in size
+                    //step 3.1 swapping of condition if the if statement is true
+                    System.out.println("Swapping ----> " + arr[j]"with--->" + arr[j + 1]);
+                    //Swapping operation performed below
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+
+                } else {
+                    System.out.println("No swap");
+
+                }
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println("after" + (i + 1) + "pass");
+            System.out.println(Arrays.toString(arr));
+
+
         }
     }
 }
